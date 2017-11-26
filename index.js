53f65c18a543c2ae5bf18e2b60a0dbf0e3e3cf71
canaanmckenzie <cjmckenz@uvm.edu>
2017-11-26 15:39:35 -0500

    update commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//Canaan McKenzie 2017
+
+const request = require('request');
+const argv = require('yargs').argv;
+let city = argv.c || 'Burlington';
+
+let apiKey = '7075dec4c77f8eeb5b0fafaa56a4716c';
+
+let url  = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+
+request(url, function (err, response, body) {
+  if(err){
+
+    console.log('error:', error);
+ 
+ } else {
+
+	//console.log('body:', body);
+        let weather = JSON.parse(body);
+	let message = `Its ${weather.main.temp} degrees Fahrenheit in ${weather.name}!`;
+	console.log(message);
+  }
+});
+
